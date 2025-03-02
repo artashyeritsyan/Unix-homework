10f6232d47829f59d08da43786a3059a51c8e952
artashyeritsyan <yeritsyanartash@gmail.com>
2025-03-02 22:44:51 +0400

    Add input validation, and seperate implementation from input

--- Homework_1/problem_1.c
+++ Homework_1/problem_1.c
@@ -4,18 +4,20 @@
 #include <sched.h>
 #include <sys/wait.h>
 
-void calculateRectangleArea() {
-    float length;
-    float width;
-    printf("Input the params for Rectangle` \n");
-    printf("\tEnter length: ");
-    scanf("%f", &length);
+float getInput() {
+    float input;
+    do {
+        if (scanf("%f", &input) != 1 || input < 1) {
+            printf("\tInvalid input. Enter again");
+        }
+    } while (input <= 0);
+    return input;
+}
 
-    printf("\tEnter width: ");
-    scanf("%f", &width);
-
+float calculateRectangleArea(float length, float width) {
     float area = length * width;
-    printf("\tRectangle Area: %.2f\n", area);
+
+    return area;
 }
 
 int main() {
@@ -23,20 +25,36 @@ int main() {
 
     if (pid < 0) {
         perror("fork error \n");
+
         return 1;
     } 
     else if (pid == 0) {
         execl("./triangleArea", "triangleArea", NULL);
         perror("Exec failed\n");
+
         return 1;
-    } else {
-        int status;
+    } 
+    else {
+        int status = 0;
         waitpid(pid, &status, 0);
+
         if (status == 1) {
             perror("The Child process terminates with error \n");
         }
 
-        calculateRectangleArea();
+        float length;
+        float width;
+
+        printf("Input the params for Rectangle` \n");
+        printf("\tEnter length: ");
+        length = getInput();
+
+        printf("\tEnter width: ");
+        width = getInput();
+
+        float area = calculateRectangleArea(length, width);
+        printf("\tRectangle Area: %.2f\n", area);
+
     }
 
     return 0;
--- Homework_1/triangleArea.c
+++ Homework_1/triangleArea.c
@@ -1,21 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void triangleArea() {
-    float base;
-    float height;
-    printf("Input the params for Triangle` \n");
-    printf("\tEnter base size: ");
-    scanf("%f", &base);
-
-    printf("\tEnter height: ");
-    scanf("%f", &height);
-
-    float area = 0.5 * base * height;
-    printf("\ttriangle Area: %.2f\n", area);
+float getInput() {
+    float input;
+    do {
+        if (scanf("%f", &input) != 1 || input < 1) {
+            printf("\tInvalid input. Enter again");
+        }
+    } while (input <= 0);
+    return input;
 }
 
+float triangleArea(float base, float height) {
+    float area = 0.5 * base * height;
+
+    return area;
+}
+
+
 int main() {
-    triangleArea();
+    float base;
+    float height;
+    
+    printf("Input the params for Triangle` \n");
+    printf("\tEnter base size: ");
+    base = getInput();
+
+    printf("\tEnter height: ");
+    height = getInput();
+
+    float area = triangleArea(base, height);
+    printf("\tTriangle Area: %.2f\n", area);
+    
     return 0;
 }
