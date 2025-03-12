76e3308033bd07b814b03d3b4f8b393d0cae0796
artashyeritsyan <yeritsyanartash@gmail.com>
2025-03-13 01:50:21 +0400

    Refactor stack_dump function and add input validation for backtrace buffer size

--- Homework_2/backtrace.cpp
+++ Homework_2/backtrace.cpp
@@ -3,9 +3,34 @@
 #include <cxxabi.h>
 #include <cstdlib>
 
+int input() {
+    int value;
+    do {
+        std::cout << "Input the size of backtraced buffer: ";
+
+        try {
+            std::cin >> value;
+            if (value <= 0) {
+                std::cerr << "Error: The size must be greater than 0." << std::endl;
+            }
+        }
+        catch (const std::exception& e) {
+            std::cerr << "Exception: " << e.what() << std::endl;
+            value = -1;
+        }
+        catch (...) {
+            std::cerr << "Unknown exception occurred." << std::endl;
+            value = -1;
+        }
+    } while (value <= 0);
+
+    return value;
+}
+
+
 void stack_dump(int depth, std::ostream& os) {
-    void* buffer[depth + 1];
-    int nptrs = backtrace(buffer, depth + 1);
+    void* buffer[depth];
+    int nptrs = backtrace(buffer, depth);
     
     char** strings = backtrace_symbols(buffer, nptrs);
     if (strings == nullptr) {
@@ -14,37 +39,27 @@ void stack_dump(int depth, std::ostream& os) {
     }
 
     for (int i = 1; i < nptrs; ++i) {
-        char* mangledName = nullptr;
-        char* offset = nullptr;
+        std::string symbol(strings[i]);
+        size_t begin = symbol.find('(');
+        size_t end = symbol.find('+');
+        size_t offset_end = symbol.find(')');
 
-        for (char* p = strings[i]; *p; ++p) {
-            if (*p == '(') {
-                mangledName = p + 1;
-            } 
-            else if (*p == '+') {
-                offset = p;
-            } 
-            else if (*p == ')' && offset) {
-                *p = '\0';
-                break;
-            }
-        }
 
-        if (mangledName && offset) {
-            *offset++ = '\0';
+        if ((begin != std::string::npos) && (end != std::string::npos) && (offset_end != std::string::npos)) {
+            std::string mangledName = symbol.substr(begin + 1, end - begin - 1);
+            std::string offset = symbol.substr(end + 1, offset_end - end - 1);
 
             int status;
-            char* demangledName = abi::__cxa_demangle(mangledName, nullptr, nullptr, &status);
+            char* demangledName = abi::__cxa_demangle(mangledName.c_str(), nullptr, nullptr, &status);
 
             if (status == 0) {
-                os << strings[i] << " : " << demangledName << "+" << offset << std::endl;
+                os << symbol.substr(0, begin) << " :  " << demangledName << "+" << offset << std::endl;
             } else {
-                os << "Demangling failed for: " << mangledName << " with status: " << status << std::endl;
-                os << strings[i] << " : " << mangledName << "+" << offset << std::endl;
+                os << "Demangling failed for: " << symbol.substr(0, begin) << mangledName << " with status: " << status << std::endl;
             }
             free(demangledName);
         } else {
-            os << strings[i] << std::endl;
+            os << symbol << std::endl;
         }
     }
 
@@ -52,39 +67,24 @@ void stack_dump(int depth, std::ostream& os) {
 }
 
 double sum(double a, double b) {
+    stack_dump(input(), std::cout);
+
     return a + b;
 }
 
-void UPPERCASE_FUNCTION(std::string str) {
+void stringFoo(std::string str) {
     sum(1.5, 4.1);
 }
 
 void start() {
-    UPPERCASE_FUNCTION("test");
+    stringFoo("test");
 }
 
-int input() {
-    int value;
-    std::cout << "Input the size of backtraced buffer: ";
 
-    try {
-        std::cin >> value;
-    }
-    catch (const std::exception& e) {
-        std::cerr << "Exception: " << e.what() << std::endl;
-        return -1;
-    }
-    catch (...) {
-        std::cerr << "Unknown exception occurred." << std::endl;
-        return -1;
-    }
 
-    return value;
-}
-
-int main() {
+int main() { 
+    // int size = input();
     start();
-
-    stack_dump(input(), std::cout);
+    // stack_dump(size, std::cout);
     return 0;
 }
